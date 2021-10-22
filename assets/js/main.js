15d3b9a23cd834496061ffd5a817fae0008f383e
bedimcode <61248995+bedimcode@users.noreply.github.com>
2021-10-22 11:38:51 -0500

    Active link visible
    Active link visible on the first link, in mobile mode. It was not visible, corrected.

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,7 +64,7 @@ function scrollActive(){
 
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 50,
+              sectionTop = current.offsetTop - 58,
               sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
